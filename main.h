c0b5eb34004962cd18b07eafa28413b84b006fd7
amyhadassah <amyhadassahbb@gmail.com>
2023-08-22 08:06:38 -0700

    changes

--- main.h
+++ main.h
@@ -1,11 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
 
-int _putchar(char c);
 int _printf(const char *format, ...);
+int _putchar(char c);
+void print_char(va_list args);
+void print_string(va_list args);
+void print_percent(void);
 int _print_string(char *s);
+
 
 #endif /* MAIN_H */
