9ad1c6137a76bd3d52921a5874044083dc14375f
Onuoha <ifechinyereonuoha@gmail.com>
2023-08-23 02:10:00 -0700

    Modifying _primtf.c file

--- _printf.c
+++ _printf.c
@@ -1,94 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
 
 /**
- * print_char - Print a character
- * @args: va_list containing the character to print
- * @count: Current character count
+ * _printf - Format and print data
+ * @format: Format string
  *
- * Return: Updated character count
- */
-int print_char(va_list args, int count)
-{
-	int c = va_arg(args, int);
-
-	putchar(c);
-	count++;
-	return (count);
-}
-
-/**
- * print_string - Print a string
- * @args: va_list containing the string to print
- * @count: Current character count
- *
- * Return: Updated character count
- */
-int print_string(va_list args, int count)
-{
-	char *s = va_arg(args, char *);
-
-	while (*s != '\0')
-	{
-		putchar(*s);
-		s++;
-		count++;
-	}
-	return (count);
-}
-
-/**
- * _printf - Custom implementation of printf function
- * @format: Format string containing conversion specifiers
- *
- * Return: Number of characters printed
+ * Return: Number of chars printed
  */
 int _printf(const char *format, ...)
 {
+
 	va_list args;
-	int count = 0;
 
 	va_start(args, format);
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
+	int print_count = 0;
 
-			switch (*format)
+	for (int i = 0; format[i] != '\0'; i++)
+	{
+
+		if (format[i] == '%')
+		{
+
+			switch (format[i + 1])
 			{
+
 				case 'c':
-					count = print_char(args, count);
+					print_count += print_char(args);
 					break;
 
 				case 's':
-					count = print_string(args, count);
+					print_count += print_string(args);
 					break;
 
 				case '%':
-					putchar('%');
-					count++;
-					break;
-
-				default:
-					putchar('%');
-					putchar(*format);
-					count += 2;
+					print_count += print_percent(args);
 					break;
 			}
-		}
-		else
+
+		} else
 		{
-			putchar(*format);
-			count++;
+			write(1, &format[i], 1);
+			print_count++;
 		}
 
-	format++;
 	}
 
-va_end(args);
+	va_end(args);
 
-return (count);
+	return (print_count);
 }
